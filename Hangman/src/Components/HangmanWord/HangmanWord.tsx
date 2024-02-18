b23184d43309f62e7d0b16d5ff7f562dc7f2dcdc
Jasil Shan <jasilshan786@gmail.com>
2024-02-18 14:39:44 +0530

    Word component added

--- Hangman/src/Components/HangmanWord/HangmanWord.tsx
+++ Hangman/src/Components/HangmanWord/HangmanWord.tsx
@@ -1,17 +1,32 @@
-
 const HangmanWord = () => {
+  const text = "hello";
+  const guessedLetters = ["e"];
   return (
     <div
-    style={{
-      display:'flex',
-      gap:'.25rem',
-      fontSize:'6rem',
-      fontWeight:'bold',
-      textTransform:'uppercase',
-      fontFamily: 'monospace',
-    }}
-     />
-  )
-}
+      style={{
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {text.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+};
 
-export default HangmanWord
+export default HangmanWord;
